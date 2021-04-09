718b824209dd5a680dfa5b8887ed6f19abc36552
ipanzerkampfwagen <emirhangurdal@gmail.com>
2021-04-09 20:40:25 +0300

    Initial Commit

---
+++ CatalogueAu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatalogueAu
+//
+//  Created by Emir Gurdal on 9.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
